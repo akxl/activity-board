b5298192d85eb84c0bd3edb0cc9d12e43352326f
akxl <aaronleong32@gmail.com>
2018-10-31 22:10:44 +0000

    adding React frontend template

---
+++ src/main/resources/static/precache-manifest.1de6ea3ddf3299152d1952358e724371.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "343ba126a8f0675a8b02",
+    "url": "/static/js/main.343ba126.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "343ba126a8f0675a8b02",
+    "url": "/static/css/main.df1f14ee.chunk.css"
+  },
+  {
+    "revision": "0bd19babaa9d8b99e2cd77d449b0a860",
+    "url": "/index.html"
+  }
+];
